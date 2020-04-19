b03c2c3e6e8b9bf3c934494a8be11ee3b84787b8
luthfi <luthfiarifin0222@gmail.com>
2020-04-19 23:14:36 +0700

    Update preference summary

--- app/src/main/java/com/laam/laammuslim/ui/main/information/InformationFragment.kt
+++ app/src/main/java/com/laam/laammuslim/ui/main/information/InformationFragment.kt
@@ -1,10 +1,11 @@
 package com.laam.laammuslim.ui.main.information
 
 import android.os.Bundle
-import android.os.Handler
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import androidx.preference.EditTextPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.laam.laammuslim.R
 import com.laam.laammuslim.data.util.changeNavigation
@@ -14,6 +15,21 @@ class InformationFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
         setHasOptionsMenu(true)
+
+        setAyahFontSizeSummary()
+    }
+
+    private fun setAyahFontSizeSummary() {
+        findPreference<EditTextPreference>("ayah_text_preference")?.apply {
+            summary =
+                "${sharedPreferences.getString("ayah_text_preference", "18")} Font Size"
+
+            onPreferenceChangeListener =
+                Preference.OnPreferenceChangeListener { _, o ->
+                    summary = "${o.toString()} Font size"
+                    true
+                }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
